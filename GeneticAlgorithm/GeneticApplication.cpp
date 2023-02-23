a7223e56a09a5ae8b21f54d08f4aff4a253f03f5
piliff-e <philipp.evdokimov@icloud.com>
2023-02-23 17:37:02 +0300

    Class Randoms added, files renamed

---
+++ GeneticAlgorithm/GeneticApplication.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Филипп Евдокимов on 04.02.2023.
+//
+
+#include "GeneticApplication.h"
+#include "Genetic.h"
+#include <iostream>
+
+/// Ввод с клавиатуры чисел, которые в дальнейшем будут преобразованы в "родительских" особей
+void GeneticApplication::input() {
+    std::cout << "Choose two six-digit numbers:\n" << "\t1) ";
+    std::cin >> n1;
+    std::cout << "\t2) ";
+    std::cin >> n2;
+    std::cout << std::endl;
+}
+
+/// Запуск проекта Genetic_algorithm
+int GeneticApplication::run() {
+    srand(time(nullptr));
+    input();
+    Genetic evolution(n1, n2);
+    std::cout << evolution.startEvolution() << " iterations" << std::endl;
+    return 0;
+}
